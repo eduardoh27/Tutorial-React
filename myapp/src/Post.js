eda93ad09794644db091734833d7ef6d5972ce34
eduardoh27 <78046940+eduardoh27@users.noreply.github.com>
2024-09-10 10:30:27 -0500

    step 9 done

--- myapp/src/Post.js
+++ myapp/src/Post.js
@@ -1,18 +1,32 @@
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+
 function Post(props) {
+
     const renderLikes = () => {
-      if (props.likes === 0) return "Give us a like";
-      else return "Likes: " + props.likes;
+        if (props.likes === 0) return "Give us a like";
+        else return "Likes" + props.likes;
     };
-   
-   
+     
+    const handleLikes = () => {
+        console.log("Button clicked...");
+    };
+ 
     return (
-      <div>
-        <h1>Post</h1>
-        <h2>Author: {props.author}</h2>
-        <h2>Content: {props.content}</h2>
-        <h2>{renderLikes()}</h2>
-      </div>
+        <Col>
+        <Card style={{ width: "18rem" }}>
+            <Card.Body className="mb-3">
+            <Card.Title>{props.author}</Card.Title>
+            <Card.Text>{props.content}</Card.Text>
+            <Card.Text>{renderLikes()}</Card.Text>
+            <Button variant="primary" onClick={handleLikes}>
+                Like
+            </Button>
+            </Card.Body>
+        </Card>
+        </Col>
     );
-   }
-   
-   export default Post;
+}
+
+export default Post;
