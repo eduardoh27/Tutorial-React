281e12fb733de4f7c64bd83cd81a0e5efd7f4c08
eduardoh27 <78046940+eduardoh27@users.noreply.github.com>
2024-09-10 10:20:52 -0500

    step 7 done

--- myapp/src/Post.js
+++ myapp/src/Post.js
@@ -1,10 +1,16 @@
 function Post(props) {
+    const renderLikes = () => {
+      if (props.likes === 0) return "Give us a like";
+      else return "Likes: " + props.likes;
+    };
+   
+   
     return (
       <div>
         <h1>Post</h1>
         <h2>Author: {props.author}</h2>
         <h2>Content: {props.content}</h2>
-        <h2>Likes: {props.likes}</h2>
+        <h2>{renderLikes()}</h2>
       </div>
     );
    }
